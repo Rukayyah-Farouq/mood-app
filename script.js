c14f5acb7b73ef18f407101ebcac4865e5488310
Rukayyah Farouq <faroukrukayyah@gmail.com>
2025-11-04 11:59:21 +0000

    moodapp

---
+++ script.js
@@ -0,0 +1,14 @@
+let happy = document.getElementById('happy')
+let sad = document.getElementById('sad')
+let images = document.getElementById('Images')
+let app = document.getElementById('app')
+let text = document.getElementById('text')
+happy.addEventListener('click', function(){
+    images.style.backgroundImage = 'url("Happy\ Pixel\ GIF\ by\ Smolverse.gif")'
+})
+sad.addEventListener('click', function(){
+    images.style.backgroundImage = 'url("Sad\ Pixel\ GIF\ by\ Potatozzz\ by\ 9GAG.gif")'
+})
+angry.addEventListener('click', function(){
+    images.style.backgroundImage = 'url("Sad\ Pixel\ GIF\ by\ Potatozzz\ by\ 9GAG.gif")'
+})
